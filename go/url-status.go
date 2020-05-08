5aee1a110e0c4b9ed860f4b34c66d0a5600a0813
Dan <44279121+dsmith73@users.noreply.github.com>
2020-05-08 07:42:35 -0400

    Create url-status.go

---
+++ go/url-status.go
@@ -0,0 +1,34 @@
+ package main
+
+// check url status response code  
+
+ import (
+ 	"fmt"
+ 	"net/http"
+ 	"os"
+ )
+
+ func main() {
+
+ 	if len(os.Args) != 2 {
+ 		fmt.Printf("Usage : %s <URL> \n", os.Args[0])
+ 		os.Exit(0)
+ 	}
+
+ 	url := os.Args[1]
+
+ 	fmt.Println("Heading ", url)
+
+ 	client := &http.Client{}
+
+ 	resp, err := client.Head(url)
+
+ 	if err != nil {
+ 		fmt.Println(err)
+ 		os.Exit(-1)
+ 	}
+
+ 	fmt.Println(url, " status is : ", resp.Status)
+
+ }
+ 
